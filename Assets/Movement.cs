2da2fe2aff9321bb849c6ff15acc2f4994ae238c
jmacasa <jmacasa@ucsd.edu>
2022-10-22 19:46:34 -0700

    my commit

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -11,10 +11,13 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         float x = Input.GetAxis("Horizontal");
-
-        Vector3 move = transform.right * x;
-
-        controller.Move(move * speed * Time.deltaTime);
+        
+        Vector3 move = transform.position;
+        move.x = Mathf.Clamp(move.x, -7.4f, 7.4f);
+        transform.position = move;
+        
+        //controller.Move(move * speed * Time.deltaTime);
     }
 }
