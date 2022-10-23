6897c94d57f7def9276cd7967b59d8a4548459da
jkleejr <jklee@ucsd.edu>
2022-10-22 19:40:26 -0700

    my 2nd

--- Assets/SpawnItems.cs
+++ Assets/SpawnItems.cs
@@ -18,7 +18,7 @@ public class SpawnItems : MonoBehaviour
             Vector3 SpawnPos = new Vector3(Random.Range(-horizontalVariation, horizontalVariation),
                 transform.position.y, 0);
             
-            GameObject spawnedItem = Instantiate(items[Random.Range(0, items.Length - 1)], SpawnPos, Quaternion.identity);
+            GameObject spawnedItem = Instantiate(items[Random.Range(0,items.Length-1)], SpawnPos, Quaternion.identity);
             totalTime = 0;
         }
 
